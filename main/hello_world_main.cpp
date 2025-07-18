7608ba8e0f43ef03fe9fc419ff2cae51fc2eda7c
MKozuch <michal.kozuch@yahoo.com>
2025-07-19 01:37:59 +0200

    Added rtos task using std api

--- main/hello_world_main.cpp
+++ main/hello_world_main.cpp
@@ -18,6 +18,7 @@
 #include "driver/uart.h"
 #include <thread>
 #include <chrono>
+#include <initializer_list>
 
 constexpr auto LED_BLUE_PIN = gpio_num_t::GPIO_NUM_4;
 constexpr auto LED_GREEN_PIN = gpio_num_t::GPIO_NUM_5;
@@ -25,13 +26,17 @@ constexpr auto LED_RED_PIN = gpio_num_t::GPIO_NUM_6;
 
 constexpr auto TAG = "asdf";
 
+
 [[noreturn]] void task1(void *args)
 {
     std::ignore = args;
 
     while (true)
     {
-        std::cout << "Hello world!" << std::endl;
+        gpio_set_level(LED_RED_PIN, 1);
+        std::cout << "Hello world from task 1!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        gpio_set_level(LED_RED_PIN, 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
@@ -42,32 +47,61 @@ constexpr auto TAG = "asdf";
 
     while (true)
     {
-        std::cout << "Hello world!" << std::endl;
+        gpio_set_level(LED_BLUE_PIN, 1);
+        std::cout << "Hello world from task 2!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        gpio_set_level(LED_BLUE_PIN, 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
 
-extern "C" {
-[[noreturn]] void app_main(void)
-    {
-        gpio_reset_pin(LED_RED_PIN);
-        gpio_set_direction(LED_RED_PIN, GPIO_MODE_OUTPUT);
+void task3(){
+    while(true){
+        std::cout << "Hello from task 3" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        gpio_set_level(LED_GREEN_PIN, 0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        gpio_set_level(LED_GREEN_PIN, 1);
+    }
+}
 
+
+extern "C" {
+//[[noreturn]]
+ void app_main(void)
+    {
+        for(auto pin : {LED_BLUE_PIN, LED_GREEN_PIN, LED_RED_PIN}){
+            gpio_reset_pin(pin);
+            gpio_set_direction(pin, GPIO_MODE_OUTPUT);
+            gpio_set_level(pin, 0);
+        }
+        
         TaskHandle_t xHandle = nullptr;
 
         xTaskCreate(
             task1,
-            "HelloTask",
+            "HelloTask2",
             1024,
             nullptr,
             10,
             &xHandle
         );
 
-        while(true)
-        {
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            std::cout << "Main thread ping" << std::endl;
-        }
-    }
+        xTaskCreate(
+            task2,
+            "HelloTask2",
+            1024,
+            nullptr,
+            10,
+            &xHandle
+        );
+
+        auto t = std::thread(&task3);
+
+       while(true)
+       {
+           vTaskDelay(5000 / portTICK_PERIOD_MS);
+           std::cout << "Main thread ping" << std::endl;
+       }
+  }
 }
